e2d987b458b6d540cc2dcc3aa52e0895b575fe5f
Benja02 <54460514+Benja02@users.noreply.github.com>
2020-08-22 14:25:49 -0600

    Ejercicio2 guia02

---
+++ Guia02/Ejercicio2/eje2.js
@@ -0,0 +1,5 @@
+var Spider = {
+    name: 'Peter Parket',
+    poderes: ["trepar", "fuerza", "agilidad", "telas de araña"]
+};
+console.log(Spider);
---
+++ Guia02/Ejercicio2/eje2.ts
@@ -0,0 +1,11 @@
+interface Spiderman{
+    name:string;
+    poderes:Array<String>;
+}
+
+let Spider:Spiderman={
+    name : 'Peter Parket',
+    poderes : ["trepar", "fuerza", "agilidad", "telas de araña"]
+}
+
+console.log(Spider);
